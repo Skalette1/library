2533399734635441111a4cf7a56d174173b10759
Beslan <beslandudaev8@gmail.com>
2024-12-29 23:32:22 +0300

    second

--- script.js
+++ script.js
@@ -1,69 +1,83 @@
-const body = document.body;
-const tittle = document.getElementById('tittle');
-const sun = document.getElementById('sun');
-const modal = document.querySelector('.modal');
-const add = document.getElementById('add');
-const public = document.getElementById('public')
 const container = document.querySelector('.container');
-const header = document.getElementById('head');
 const library = document.querySelector('.library');
-let nameBook = document.getElementById('name')
-let authorBook = document.getElementById('author')
-let pagesBook = document.getElementById('pages')
+const shelf = document.querySelector('.shelf')
+const modalWindow = document.querySelector('.modal');
+const nameInput = document.getElementById('name');
+const authorInput = document.getElementById('author');
+const pagesInput = document.getElementById('pages');
+const addBook = document.getElementById('add');
+const publicBook = document.getElementById('public');
+const checkBox = document.getElementById('check')
 
-
-sun.addEventListener('click', () => {
-    if(body.style.background === 'rgb(255, 255, 255)') {
-    body.style.cssText = 'background: #000; color: #fff';
-    tittle.style.cssText = 'text-shadow: 0px 0px 5px #fff;'
-    modal.style.cssText = 'color: black'
-    sun.innerHTML = '<img src="moon_stars_night_midnight_sky_icon_194278.png" alt="" height="30" width="30">'
-    header.style.cssText = 'box-shadow: 0 0 15px #fff;'
-    }
-    else {
-    body.style.cssText = 'background: #fff; color: #000'
-    modal.style.cssText = 'color: black; box-shadow: 1px 1px 20px #fff'
-    tittle.style.cssText = 'text-shadow: 0px 0px 5px #000';
-    sun.innerHTML = '<img src="sun_sunny_summer_icon-icons.com_62700 (1).png" alt="" height="30" width="30">'
-        header.style.cssText = 'box-shadow: 0 0 15px #000;'
-    }
-})
-
-add.addEventListener('click', () => {
-    modal.style.cssText = 'opacity: 1; display: flex; flex-direction: column; justify-content: center;align-items: center;text-align: center;gap: 1.2rem;';
+addBook.addEventListener('click', () => {
+    modalWindow.style.cssText = 'opacity: 1; display: flex; flex-direction: column; justify-content: center;align-items: center;text-align: center;gap: 1.2rem;';
     container.style.cssText = 'z-index: .7; opacity: .3';
 })
 
-const myLibrary = ['us', 'tis', 33, 'read', 'remove'];
-
-const shelf = document.createElement('div');
-library.appendChild(shelf);
-shelf.style.cssText = 'display: flex; flex-direction: column; align-items: center; gap: 2rem;min-width: 20rem; min-height: 10rem; box-shadow: 0 0 15px #000;';
-
-myLibrary.forEach(books => {
-    const element = document.createElement('p');
-    element.textContent = books;
-    shelf.appendChild(element);
-})
+const myLibrary = [];
 
 function Book(name, author, pages, isRead) {
     this.name = name;
     this.author = author;
     this.pages = pages;
-    this.isRead = isRead
+    this.isRead = isRead;
 }
+
+function findElement() {
+  library.textContent = '';
+  myLibrary.forEach(books => {
+    const element = document.createElement('div');
+    element.innerHTML = `<h3>${books.name}</h3>
+    <p>${books.author}</p>
+    <p>${books.pages}</p>
+    <button id='toogleRead' onclick="toogleBtn()">${books.isRead}</button>
+    <button id='remove' oncklick = 'removeBook()'>Удалить книгу</button>
+    `
+    library.appendChild(element);
+    element.classList.add('shelf')
+})
+}
+
+function toogleBtn() {
+  if(toogleRead.textContent === 'Прочтена') {
+    toogleRead.textContent = 'Не прочитана';
+    toogleRead.style.background = '#c60000'
+    }
+    else {
+    toogleRead.textContent = 'Прочтена';
+    toogleRead.style.background = 'green'
+    }
+}
+
 
 function putBook() {
-const name = nameBook.value;
-const author = authorBook.value;
-const pages = pagesBook.value;
+const name = nameInput.value;
+const author = authorInput.value;
+const pages = parseInt(pagesInput.value);
+const isRead = checkBox.checked? 'Прочтена' : 'Не прочитана';
+const backgroundColor = checkBox.checked ? '#c60000' : 'green';
+   if(name && author && !isNaN(pages)) {
+    const newBook = new Book(name, author, pages, isRead);
+    myLibrary.push(newBook);
+    findElement()
+   }
+   else {
+     alert("Заполните все бланки или введите корректные значения")
+  }
 }
 
+function removeBook() {
+  newBook.remove();
+}
 
-public.addEventListener('click', () => {
-    modal.style.display = 'none';
+publicBook.addEventListener('click', () => {
+    putBook();
+    modalWindow.style.display = 'none';
     container.style.cssText = 'z-index: 1; opacity: 1';
-    nameBook.value = '';
+    nameInput.value = '';
+    authorInput.value = '';
+    pagesInput.value = '';
+    checkBox.checked = false
 })
 
 
@@ -71,11 +85,10 @@ public.addEventListener('click', () => {
 
 
 
-// const bookDiv = document.createElement('div')
-// library.appendChild(bookDiv)
+
+// const read = document.createElement('button');
+// read.textContent = "read";
+// read.style.cssText = 'background: red'
+// myLibrary.push(read);
 
 
-const read = document.createElement('button');
-read.textContent = "read";
-read.style.cssText = 'background: red'
-myLibrary.push(read);
